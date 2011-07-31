32ee7459bb89bba01d7bd56ea491e9203cf4e306
Avinasha Shastry <avinasha1987@gmail.com>
2011-07-31 09:40:15 +0530

    products index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,15 +3,19 @@ class ProductsController < ApplicationController
   respond_to :html
   layout 'app'
 
+  def index
+    @products = Product.all
+  end
+
   def new
     @product = Product.new
   end
 
   def create
     @product = Product.create params[:product]
-
-    respond_with @product
-
+    respond_with @product do |format|
+      format.html { redirect_to products_path }
+    end
   end
 
 end
