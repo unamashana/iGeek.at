cb384caec2c2b03c25626ea159a82fd200c05712
Prateek Dayal <prateek@supportbee.com>
2011-07-31 11:03:25 +0530

    changes to layout/ux

--- app/controllers/product_users_controller.rb
+++ app/controllers/product_users_controller.rb
@@ -9,7 +9,11 @@ class ProductUsersController < ApplicationController
     # user_id is right now ignored
     #
     current_user.products << @product
-    respond_with @product
+    #respond_with @product
+
+    render :update do |page|
+      page["#list"].prepend render :partial => 'products/product', :locals => {:product => @product}
+    end
     
 
   end
--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -2,8 +2,10 @@ class SetupController < ApplicationController
 
   before_filter :authenticate
   layout 'app'
+
   def index
     @products = Product.all
+    @already_using = current_user.products
     @autocomplete_array = @products.map {|p| {:label => p.name, :value => p.id} }
   end
 end
